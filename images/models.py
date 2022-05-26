8c8b65bf19ca41ad0ebeebb8bed48206b96e17d0
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-25 23:54:25 -0400

    added Image,Location and Category models in models.py

--- images/models.py
+++ images/models.py
@@ -1,6 +1,18 @@
+from unicodedata import category
 from django.db import models
-from django.forms import CharField
+import datetime as dt
 
 # Create your models here.
-class Image():
-    name=CharField
+class Location(models.Model):
+     name=models.CharField(max_length=225)
+
+class Category(models.Model):
+     name=models.CharField(max_length=225)
+
+class Image(models.Model):
+    name=models.CharField(max_length=225)
+    description=models.CharField(max_length=225)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to = '/', Default="")

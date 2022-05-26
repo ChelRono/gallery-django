a85bd791d9f49e8cecf6c3ed1027431520b9c351
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-25 22:26:49 -0400

    added index.html to display random photos

--- images/views.py
+++ images/views.py
@@ -1,5 +1,12 @@
+from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image
+
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the Moringa Tribune')
+def index(request):
+    '''
+    Index function loads the start up page with a filter of randon six images at the beginning
+    '''
+    gallery = Image.objects.all()[:6]
+    return render(request,'index.html', {'gallery':gallery})

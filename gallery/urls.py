c8b8b3e85669e3488ee54310c96c92b23a6f7d13
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-05-27 22:50:11 -0400

    imported settings and static

--- gallery/urls.py
+++ gallery/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import re_path as  url,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('images.urls'))
     
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
